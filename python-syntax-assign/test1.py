74f2f38d94d27a4296d896f75f517cb08d08b82a
vampyricicarus <darbiicarus@gmail.com>
2024-08-31 18:07:06 -0400

    first commit

---
+++ python-syntax-assign/test1.py
@@ -0,0 +1,18 @@
+# task 1
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+# the error in the code was that the indents were missing
+
+# task 2
+
+feeling = input("How are you feeling today? ")
+if feeling == "happy":
+    print("That's great to hear!")
+elif feeling == "sad":
+    print("I hope your day gets better!")
+
---
+++ python-syntax-assign/test3.py
@@ -0,0 +1,12 @@
+# task 1
+
+bread, peanutButter, jelly = 5, 2, 3
+sandwich = bread + peanutButter + jelly
+print(sandwich)
+# I created a price for each item then added them together for a final price
+
+# task 2
+
+savings = 10000 * 1.07
+print(savings)
+# this one was simple math
