4ff4f94f7ffe9709dcef554626181473ea4c776c
Darbi <darbiicarus@gmail.com>
2024-09-01 19:43:42 -0400

    test2.py

---
+++ python-syntax-assign/test2.py
@@ -0,0 +1,10 @@
+# task 1
+
+PI = 3.14
+userAge = input("How old are you?")
+userLocation = "New York"
+MAXLIMIT = 1000
+# PI is a constant and should be capitalized
+# user age should be based on input
+# user location should be in camel case
+# the final constant looks correct
